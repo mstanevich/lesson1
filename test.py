670645914b5467adc7ef8207901eceea5df4da80
unknown <stanevich1995@gmail.com>
2025-12-22 18:51:22 +0300

    lesson1 done

---
+++ test.py
@@ -0,0 +1,39 @@
+"""
+a = 3
+b = "text"
+
+
+c = 9
+
+res:int = a +c
+res2 = res / 3
+#print(type(res2))
+
+lynxenko = "lysenko masha"
+"""
+# name = "Masha    "
+# surname = "    Lysenko"
+# fullname = name.strip() + surname
+# print(fullname)
+
+# first_name = "masha"
+# last_name = "lysenko"
+# full_name = f"Hello, {first_name} {last_name}"
+# print(full_name.title())
+
+"""СПИСКИ"""
+
+a = ["biba", "BOBA", "Zhenya"]
+print(a[2].upper())
+
+"""вывести сообщение "мой друг" имя с большой буквы """
+b = []
+b.append("znach1")
+b.append("znach2")
+b.insert(0, "znach0")
+# b.pop(1)
+del b[0]
+print(b)
+
+"""метод remove чтобы удалить элемент списка, позиция которого неизвестна"""
+"""вывести с помощью f строки значение с большой буквы"""
